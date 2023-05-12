6c03d17ca91ba9a1fbc1ee98615284e4b611427a
ahmadjamalkhater <ahmadjamalkhater@gmail.com>
2023-05-12 15:51:49 +0000

    i fix the emplimintaion error

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Movies-Library1
+
+# Project Name - Movies-Library - Project Version
+
+**Author Name**: ahmad khater
+
+## WRRC
+
+![WRRC](./WRRCDB.jpg);
+
+## Overview
+
+## Getting Started
+* ### first step you have to clone this code ssh from this repo
+
+* ### install required files by using `npm init -y`
+
+* ### install express files `npm i express`
+
+* ### to run server just use `npm start`
+
+* ### and then go to  `http://localhost:3000/` you will get the main page of server Note: server down untill you turn it on as i mentioned before
+
+* ### there is 2 routes for now 1 is `/` nad the second one is `/favorite`
+
+## Project Features
+## you have some packges to install to deal with this app 
+
+* ### install dotenv package `npm install dotnev` 
+
+* ### install axios package `npm install axios`
+
+
+* ### install cors package `npm install cors`
---
+++ server.js
@@ -0,0 +1,50 @@
+const data =require('./Movie Data/data.json')
+const express=require('express');
+const server=express();
+const cors = require('cors');
+const PORT=3000;
+
+const status500 = {
+    "status": 500,
+    "responseText": "Sorry, something went wrong"
+};
+
+const status404 = {
+    "status": 404,
+    "responseText": "Sorry, page not found error"
+};
+
+server.use(cors()) ;
+
+server.get('/', (req, res) => {
+    const movie = new Data(data.title,data.poster_path,data.overview);
+
+    res.send(movie);
+})
+server.get('/favorite', (req, res) => {
+    res.send("Welcome to Favorite Page");
+})
+
+server.get('/error', (req, res) => {
+    res.send(error());
+})
+
+server.use(function (req, res) {
+    res.status(404).send(status404);
+});
+
+function Data(title,poster_path,overview) {
+    this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+server.use(function (error, req, res, next) {
+    res.status(500).send(status500);
+});
+
+
+
+
+server.listen(PORT, () => {
+console.log(`Server listening on port ${PORT}:Iam ready`)});
